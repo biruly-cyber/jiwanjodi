7a0b287ba57c4e8cb0a17a33e12d0723018c5b9b
biruly-cyber <biruly2000@gmail.com>
2024-06-27 00:29:30 +0530

    trying to configure redux

--- src/redux/actions/userAction.ts
+++ src/redux/actions/userAction.ts
@@ -1,39 +1,40 @@
-import { SERVER_URL } from "@/serverConfig";
-import axios from "axios";
-import { AppDispatch } from "../store";
-import { toast } from "@/components/ui/use-toast";
 
-// Create new account handler -- Working
-export const registrationAction =
-  (email:string, password:string) => async (dispatch: AppDispatch) => {
-    try {
-      dispatch({
-        type: "SEND_OTP_FOR_REGISTRATION_REQUEST",
-      });
+// import { SERVER_URL } from "@/serverConfig";
+// import axios from "axios";
+// // import { AppDispatch } from "../store";
+// import { toast } from "@/components/ui/use-toast";
 
-      const { data } = await axios.post(
-        `${SERVER_URL}/user/new`,
-        {email, password}
-      );
+// // Create new account handler -- Working
+// export const registrationAction =
+//   (email:string, password:string) => async (dispatch: AppDispatch) => {
+//     try {
+//       dispatch({
+//         type: "SEND_OTP_FOR_REGISTRATION_REQUEST",
+//       });
 
-      dispatch({
-        type: "SEND_OTP_FOR_REGISTRATION_SUCCESS",
-        payload: { message: data?.message },
-      });
-      toast({
-        title: "Success",
-        description: data?.message,
-        variant: "default",
-      });
-    } catch (e: any) {
-      dispatch({
-        type: "SEND_OTP_FOR_REGISTRATION_FAIL",
-        payload: { error: e?.response?.data?.message || e?.message },
-      });
-      toast({
-        title: "Error",
-        description: e?.response?.data?.message || e?.message,
-        variant: "destructive",
-      });
-    }
-  };
+//       const { data } = await axios.post(
+//         `${SERVER_URL}/user/new`,
+//         {email, password}
+//       );
+
+//       dispatch({
+//         type: "SEND_OTP_FOR_REGISTRATION_SUCCESS",
+//         payload: { message: data?.message },
+//       });
+//       toast({
+//         title: "Success",
+//         description: data?.message,
+//         variant: "default",
+//       });
+//     } catch (e: any) {
+//       dispatch({
+//         type: "SEND_OTP_FOR_REGISTRATION_FAIL",
+//         payload: { error: e?.response?.data?.message || e?.message },
+//       });
+//       toast({
+//         title: "Error",
+//         description: e?.response?.data?.message || e?.message,
+//         variant: "destructive",
+//       });
+//     }
+//   };
